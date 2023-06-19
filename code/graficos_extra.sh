91230d06cfbe041196f6cc11cf12af4f74762429
Erich Grüttner <egruttner@gmail.com>
2023-06-19 16:34:48 -0400

    Entrega Final

--- code/graficos_extra.sh
+++ code/graficos_extra.sh
@@ -15,13 +15,20 @@ python3 plot_dos.py csv/search_in_hash_abierto_user_id_results.csv csv/search_ou
 python3 plot_dos.py csv/search_in_hash_cerrado_user_id_results.csv csv/search_out_hash_cerrado_user_id_results.csv Búsqueda_FOUND Búsqueda_FAIL "Rendimiento Búsqueda Hash Cerrado - User_Id"
 
 
+python3 plot_dos.py csv/search_in_hash_abierto_user_id_results.csv csv/search_in_hash_cerrado_user_id_results.csv Hash-Abierto Hash-Cerrado "Comparación búsquedas IN Tablas Hash - User_Id"
+python3 plot_dos.py csv/search_in_hash_abierto_user_name_results.csv csv/search_in_hash_cerrado_user_name_results.csv Hash-Abierto Hash-Cerrado "Comparación búsquedas IN Tablas Hash - User_Name"
 
 
 
-python3 plot_tres.py csv/insert_arbol_user_id_results.csv csv/search_in_arbol_user_id_results.csv csv/search_out_arbol_user_id_results.csv Inserción Búsqueda_FOUND Búsqueda_FAIL "Rendimiento Operación Árbol - User Id"
 
 
-python3 plot_tres.py csv/insert_arbol_user_id_results.csv csv/search_in_arbol_user_id_results.csv csv/search_out_arbol_user_id_results.csv Inserción Búsqueda_FOUND Búsqueda_FAIL "Rendimiento Operación Árbol - User Id"
+
+
+python3 plot_tres.py csv/search_in_arbol_user_id_results.csv csv/search_in_hash_abierto_user_id_results.csv csv/search_in_hash_cerrado_user_id_results.csv Árbol Hash-Abierto Hash-Cerrado "Comparación Búsqueda FOUND User_Id"
+python3 plot_tres.py csv/search_in_arbol_user_name_results.csv csv/search_in_hash_abierto_user_name_results.csv csv/search_in_hash_cerrado_user_name_results.csv Árbol Hash-Abierto Hash-Cerrado "Comparación Búsqueda FOUND User_Name"
+
+python3 plot_tres.py csv/insert_arbol_user_id_results.csv csv/insert_hash_abierto_user_id_results.csv csv/insert_hash_cerrado_user_id_results.csv Árbol Hash-Abierto Hash-Cerrado "Comparación Inserción User_Id"
+python3 plot_tres.py csv/insert_arbol_user_name_results.csv csv/insert_hash_abierto_user_name_results.csv csv/insert_hash_cerrado_user_name_results.csv Árbol Hash-Abierto Hash-Cerrado "Comparación Inserción User_Name"
 
 
 
